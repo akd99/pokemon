b46ec718501b7d8d72570d5d8ea368090fe3293d
akd <add99man2@yahoo.com>
2021-04-25 17:06:42 -0500

    test

--- dashboard/decks.js
+++ dashboard/decks.js
@@ -1,6 +1,6 @@
 const leftDeckRep = nodecg.Replicant('leftDeck');
 const rightDeckRep = nodecg.Replicant('rightDeck');
-//test
+//testdfdfd
 function updateDeck() {
     const left_deckInput = document.getElementById('left_deck');
     const right_deckInput = document.getElementById('right_deck');
