2b308c1e2071ba4721861bb2829bb663e122569f
akd <add99man2@yahoo.com>
2021-05-01 11:45:01 -0500

    fix

--- extension/index.js
+++ extension/index.js
@@ -1,7 +1,5 @@
 'use strict';
 const axios = require('axios');
-const pokemon = require('pokemontcgsdk');
-pokemon.configure({apiKey: 'f4611634-9596-4386-9d93-9b55e200f91d'});
 module.exports = function (nodecg) {
 	const pokemonCardResultsReplicant = nodecg.Replicant('pokemon-card-results');
 	nodecg.listenFor('pokemonCardSearch', async nameSearch =>{
