176cc5b7e3448f75fb0dbce5786c14cdf580849d
akd <add99man2@yahoo.com>
2021-05-01 14:30:56 -0500

    prizes fix

--- graphics/prizes.js
+++ graphics/prizes.js
@@ -17,106 +17,106 @@ const p2prizeRep = nodecg.Replicant('p2Prize');
 
 p1prizeRep.on('change', (newVal) => {
     if (newVal == 0) {
-        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize6El.innerHTML = "<img src=pokeballbw.png width=50 height=50>";
-    } else if (newVal == 1) {
-        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize6El.innerHTML = "<img src=pokeballbw.png width=50 height=50>";
-    } else if (newVal == 2) {
-        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize6El.innerHTML = "<img src=pokeballbw.png width=50 height=50>";
-    } else if (newVal == 3) {
-        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize6El.innerHTML = "<img src=pokeballbw.png width=50 height=50>";
-    } else if (newVal == 4) {
-        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>";
-        leftPrize6El.innerHTML = "<img src=pokeballbw.png width=50 height=50>";
-    } else if (newVal == 5) {
-        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>";
-        leftPrize6El.innerHTML = "<img src=pokeballbw.png width=50 height=50>";
-    } else if (newVal == 6) {
         leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
         leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
         leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
         leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
         leftPrize5El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>";
         leftPrize6El.innerHTML = "<img src=pokeballred.png width=50 height=50>";
+    } else if (newVal == 1) {
+        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize6El.innerHTML = "<img src=pokeballred.png width=50 height=50>";
+    } else if (newVal == 2) {
+        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize6El.innerHTML = "<img src=pokeballred.png width=50 height=50>";
+    } else if (newVal == 3) {
+        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize6El.innerHTML = "<img src=pokeballred.png width=50 height=50>";
+    } else if (newVal == 4) {
+        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>";
+        leftPrize6El.innerHTML = "<img src=pokeballred.png width=50 height=50>";
+    } else if (newVal == 5) {
+        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize6El.innerHTML = "<img src=pokeballred.png width=50 height=50>";
+    } else if (newVal == 6) {
+        leftPrize1El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize2El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize3El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize4El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize5El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        leftPrize6El.innerHTML = "<img src=pokeballbw.png width=50 height=50>";
     }
 })
 
 p2prizeRep.on('change', (newVal) => {
     if (newVal == 0) {
-        rightPrize1El.innerHTML = "<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
-        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;";
-        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;";
-        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        rightPrize1El.innerHTML = "<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
+        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;";
+        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;";
+        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>";
     } else if (newVal == 1) {
-        rightPrize1El.innerHTML = "<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
-        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;";
-        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;";
-        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        rightPrize1El.innerHTML = "<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
+        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;";
+        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;";
+        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>";
     } else if (newVal == 2) {
-        rightPrize1El.innerHTML = "<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
-        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;";
-        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;";
-        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        rightPrize1El.innerHTML = "<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
+        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;";
+        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;";
+        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>";
     } else if (newVal == 3) {
-        rightPrize1El.innerHTML = "<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
-        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;&nbsp;";
-        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;";
-        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        rightPrize1El.innerHTML = "<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
+        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;&nbsp;";
+        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;";
+        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>";
     } else if (newVal == 4) {
-        rightPrize1El.innerHTML = "<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
-        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;";
-        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>&nbsp;";
-        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        rightPrize1El.innerHTML = "<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
+        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;";
+        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>&nbsp;";
+        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>";
     } else if (newVal == 5) {
-        rightPrize1El.innerHTML = "<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
-        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;";
-        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;";
-        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw.png width=50 height=50>";
+        rightPrize1El.innerHTML = "<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
+        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;";
+        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;";
+        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred2.png width=50 height=50>";
     } else if (newVal == 6) {
-        rightPrize1El.innerHTML = "<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
-        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
-        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;&nbsp;";
-        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>&nbsp;";
-        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballred.png width=50 height=50>";
+        rightPrize1El.innerHTML = "<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize2El.innerHTML = "&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;&nbsp;";
+        rightPrize3El.innerHTML = "&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;&nbsp;";
+        rightPrize4El.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;&nbsp;";
+        rightPrize5El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>&nbsp;";
+        rightPrize6El.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src=pokeballbw2.png width=50 height=50>";
     }
 })
